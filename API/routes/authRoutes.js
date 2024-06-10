9971702fc4ad629e8a3fdbaf9be50ead8e8ce2a4
vnq1909 <144467718+vnq1909@users.noreply.github.com>
2024-06-10 18:44:26 +0700

    Delete API directory

--- API/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {login, register} from '../controllers/authController.js'
-const router = express.Router();
-
-router.post("/login", login); //login route
-router.post("/register", register); //register route
-
-export default router;
